1fbbd17509fa8ca4f24bf458a65bce9dff124100
fukadamb1 <61417369+fukadamb1@users.noreply.github.com>
2022-07-15 01:11:19 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -5,11 +5,9 @@ window.addEventListener("DOMContentLoaded",
     // ページ本体が読み込まれたタイミングで実行するコード
 
     const item = document.querySelectorAll(".item"); // icon
-    const itemArray = Array.from(item); // Arrayに変換
-    itemArray.forEach(function(element, index) {
+    item.forEach(function(element, index) {
       // 0.2s毎にずれて表示
       setTimeout(function () {
-          element.classList.remove("opacity");
           element.classList.add("fade-in");
         }, 200 * index);
     });
